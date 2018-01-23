e4e4469d4a82e63cee95a1ac5ce48cff1a763797
Abhicoding <shinganeabhishek@gmail.com>
2018-01-23 09:01:28 +0530

    added test and initialising files

--- lispy.js
+++ lispy.js
@@ -1,2 +1,13 @@
-//Lisp interpreter project 22/01/2018 13:15
+// Lisp interpreter project 22/01/2018 13:15
 
+function parseSomething (input) {
+
+}
+
+function parseSpace (input) {
+	 let reg = /^(\s+)/, parseOut = input.match(reg)
+  if (parseOut) {
+    return [parseOut[0], input.substring(parseOut[0].length)]
+  }
+  return null
+}
