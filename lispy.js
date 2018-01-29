f935613af39673619598c5e32b91126654d3e366
Abhicoding <shinganeabhishek@gmail.com>
2018-01-29 15:09:30 +0530

    if statements are evaluated better

--- lispy.js
+++ lispy.js
@@ -7,7 +7,7 @@ function parseAble (input) {
   return input
 }
 
-const lib = {'mathops': { '+': (array) => { return array.reduce((a, b) => Number(a) + Number(b)) },
+const lib = { '+': (array) => { return array.reduce((a, b) => Number(a) + Number(b)) },
   '-': (array) => { return array.reduce((a, b) => Number(a) - Number(b)) },
   '*': (array) => { return array.reduce((a, b) => Number(a) * Number(b)) },
   '/': (array) => { return array.reduce((a, b) => Number(a) / Number(b)) },
@@ -23,7 +23,7 @@ const lib = {'mathops': { '+': (array) => { return array.reduce((a, b) => Number
     } else { throw new Error('Wrong number of arguments. Need exactly 1') }
   },
   'max': (array) => { return Math.max.apply(null, array.map(Number)) },
-  'min': (array) => { return Math.min.apply(null, array.map(Number)) }},
+  'min': (array) => { return Math.min.apply(null, array.map(Number)) },
   'begin': (array) => { return array.pop() },
   'print': (array) => { return console.log(array.join('')) },
   'if': (array) => {
@@ -33,12 +33,10 @@ const lib = {'mathops': { '+': (array) => { return array.reduce((a, b) => Number
     return array[2]
   },
   'define': (array) => {
-    if (array[0] in lib) {
-      throw new Error('Keyword is being overwritten')
-    } else {
-      userdef[array[0]] = array[1]
-      return userdef[array[0]]
+    if (array.length === 1) {
+      return userdef[array[0]] = undefined
     }
+    return userdef[array[0]] = array[1]
   }
 }
 
@@ -72,24 +70,37 @@ function parseLisp (input) {    // (lisp expression in string)
 }
 
 function evaluator (ops, array) {  // (operation , [arguments+])
-  if (ops in lib['mathops']) {
-    let cloneArray = []
-    for (let x of array) {
-      if (isNaN(Number(x))) {
-        if (x in userdef) {
-          cloneArray.push(userdef[x])
-          continue
-        }
-        console.log('Error: execute: unbound symbol: ' + x + ' []')
-        throw new Error()
-      }
-      cloneArray.push(x)
+  if (ops === 'define') {
+    if (isNaN(array[0])) {
+      return lib[ops](array)
     }
-    return lib['mathops'][ops](cloneArray)
+    throw new Error('Value cannot be re-assigned')
   }
-  return lib[ops](array)
+
+  if (ops === 'if') {
+    let x = lib[ops](array)
+    if (Number(x)) {
+      return x
+    }
+    console.log('Error: execute: unbound symbol: ' + x + ' []')
+    throw new Error()
+  }
+
+  let cloneArray = []
+  for (let x of array) {
+    if (isNaN(Number(x))) {
+      if (x in userdef) {
+        cloneArray.push(userdef[x])
+        continue
+      }
+      console.log('Error: execute: unbound symbol: ' + x + ' []')
+      throw new Error()
+    }
+    cloneArray.push(x)
+  }
+  return lib[ops](cloneArray)
 }
 
-// exports.interpret = parseLisp
-console.log(parseLisp('(begin (define x 2) (* x x))'))
+exports.interpret = parseLisp
+// console.log(parseLisp('(if (< (* 11 11) 120) oo123ps (* 7 6))'))
 // console.log(parseLisp('(if (> (* 11 11) 120) 1 0)'))
