22a63c5700c8a56805f7a791e9965cad14db3477
Abhicoding <shinganeabhishek@gmail.com>
2018-01-29 11:46:06 +0530

    Fixed a bug where alphabetical input would slip through and return

--- lispy.js
+++ lispy.js
@@ -7,7 +7,7 @@ function parseAble (input) {
   return input
 }
 
-const lib = {'+': (array) => { return array.reduce((a, b) => Number(a) + Number(b)) },
+const lib = {'mathops': { '+': (array) => { return array.reduce((a, b) => Number(a) + Number(b)) },
   '-': (array) => { return array.reduce((a, b) => Number(a) - Number(b)) },
   '*': (array) => { return array.reduce((a, b) => Number(a) * Number(b)) },
   '/': (array) => { return array.reduce((a, b) => Number(a) / Number(b)) },
@@ -23,16 +23,27 @@ const lib = {'+': (array) => { return array.reduce((a, b) => Number(a) + Number(
     } else { throw new Error('Wrong number of arguments. Need exactly 1') }
   },
   'max': (array) => { return Math.max.apply(null, array.map(Number)) },
-  'min': (array) => { return Math.min.apply(null, array.map(Number)) },
-  'begin': [],
-  'print': (input) => { return console.log(input) },
+  'min': (array) => { return Math.min.apply(null, array.map(Number)) }},
+  'begin': (array) => { return array.pop() },
+  'print': (array) => { return console.log(array.join('')) },
   'if': (array) => {
     if (array[0] === true) {
       return array[1]
     }
     return array[2]
+  },
+  'define': (array) => {
+    if (array[0] in lib) {
+      throw new Error('Keyword is being overwritten')
+    } else {
+      userdef[array[0]] = array[1]
+      return userdef[array[0]]
+    }
   }
 }
+
+const userdef = {}
+
 function parseLisp (input) {    // (lisp expression in string)
   if (typeof (input) === 'string') {
     input = parseAble(input)
@@ -61,12 +72,18 @@ function parseLisp (input) {    // (lisp expression in string)
 }
 
 function evaluator (ops, array) {  // (operation , [arguments+])
-  if (ops === 'define') {
-    return lib[array[1]] = array[2]
+  if (ops in lib['mathops']) {
+    for (let x of array) {
+      if (isNaN(Number(x))) {
+        console.log('Error: execute: unbound symbol: ' + x + ' []')
+        throw new Error()
+      }
+    }
+    return lib['mathops'][ops](array)
   }
   return lib[ops](array)
 }
 
 // exports.interpret = parseLisp
-console.log(parseLisp('(if (< 2 1) (+ 2 3) (+ 2 1))'))
+console.log(parseLisp('(+ 1 2)'))
 // console.log(parseLisp('(if (> (* 11 11) 120) 1 0)'))
